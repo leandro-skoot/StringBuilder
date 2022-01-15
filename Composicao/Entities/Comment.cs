a4f39356458d2ce6be4ff95bfa4b66d6f5f84709
Leandro Silva <leandro.skoot@gmail.com>
2022-01-15 17:24:55 -0300

    StringBuilder

---
+++ Composicao/Entities/Comment.cs
@@ -0,0 +1,17 @@
+﻿namespace Composicao.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
